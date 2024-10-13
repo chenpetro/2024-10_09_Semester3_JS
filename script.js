e6773a0ce3b95418a942073da7c5efd5b3c1a6a3
chenpetro <chenpetro@gmail.com>
2024-10-13 12:02:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const message = 'Welcome to Bahamas!';
+// Breaks the string by a delimiter, in this case it is a space
+// console.log(message.split(' ')[1].toUpperCase());
+// console.log(message.split(''));
+const splitMessage = message.split(' ')
+console.log(splitMessage[1].toUpperCase());
+console.log(splitMessage);
